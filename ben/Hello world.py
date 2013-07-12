884b1d75766f4b1efd3e73d069314afaeb6c1226
carterwill14 <carterwill14@gmail.com>
2013-07-12 17:21:22 -0400

    get stuff

---
+++ ben/Hello world.py
@@ -0,0 +1,23 @@
+print ('hello world')
+b=837
+print (b)
+abc=100*5
+abc=77*1
+print (abc)
+
+# this an example of a string data type
+q="500"
+print (q)
+
+# get a value from the user and display it
+name=input("name of company: ")
+print (name)
+
+# example of conditional statement
+z=25
+if z>1:
+    print ("buy")
+elif z==25:
+    print ("hold")
+else:
+    print ("sell")
---
+++ myprog.py
@@ -0,0 +1,5 @@
+print (x)
+
+#  Get a value from user and display it
+name=input("name of company: ")
+print (name)
